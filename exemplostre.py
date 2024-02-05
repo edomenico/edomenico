ee34daaaec287a13a99feadd48b917141409f466
edomenico <67431641+edomenico@users.noreply.github.com>
2024-02-04 22:03:34 -0300

    Update exemplostre.py

--- exemplostre.py
+++ exemplostre.py
@@ -331,13 +331,13 @@ def main():
 
 
         if area==1:
-            os.chdir("edomenico/edomenico/tree/28357db2ae10f1b6ff0e5ef70cbb534eaf388848/area1")
+            os.chdir("os.chdir("/mount/src/edomenico/area1")")
             # extension = 'csv'
             # all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
             # arqi1 = pd.concat([pd.read_csv(f) for f in all_filenames])
             #arqi1 = pd.read_csv('metar_trat_teste1.csv')
         else:
-            os.chdir("./area2")
+            os.chdir("os.chdir("/mount/src/edomenico/area2")")
         extension = 'csv'
         all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
         arqi1 = pd.concat([pd.read_csv(f) for f in all_filenames])
