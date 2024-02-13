ba47b474898615eff195d0102b65cfde055e9ec7
edomenico <67431641+edomenico@users.noreply.github.com>
2024-02-13 15:42:44 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -44,36 +44,28 @@ from webdriver_manager.firefox import GeckoDriverManager
 
 def main():
 
-  import streamlit as st
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.firefox import GeckoDriverManager
 
-"""
-## Web scraping on Streamlit Cloud with Selenium
+URL = ""
+TIMEOUT = 20
 
-[![Source](https://img.shields.io/badge/View-Source-<COLOR>.svg)](https://github.com/snehankekre/streamlit-selenium-chrome/)
+st.title("Test Selenium")
 
-This is a minimal, reproducible example of how to scrape the web with Selenium and Chrome on Streamlit's Community Cloud.
-
-Fork this repo, and edit `/streamlit_app.py` to customize this app to your heart's desire. :heart:
-"""
-
-with st.echo():
-    from selenium import webdriver
-    from selenium.webdriver.chrome.options import Options
-    from selenium.webdriver.chrome.service import Service
-    from webdriver_manager.chrome import ChromeDriverManager
-
-    @st.experimental_singleton
-    def get_driver():
-        return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-
-    options = Options()
-    options.add_argument('--disable-gpu')
-    options.add_argument('--headless')
-
-    driver = get_driver()
-    driver.get("http://example.com")
-
-    st.code(driver.page_source)
+firefoxOptions = Options()
+firefoxOptions.add_argument("--headless")
+service = Service(GeckoDriverManager().install())
+driver = webdriver.Firefox(
+    options=firefoxOptions,
+    service=service,
+)
+driver.get(URL)
 
 
 
