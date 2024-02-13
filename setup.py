a2aa164eee1ebaebe6f47fa566cb4633640f1590
edomenico <67431641+edomenico@users.noreply.github.com>
2024-02-13 13:13:07 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -35,8 +35,26 @@ from webdriver_manager.firefox import GeckoDriverManager
 
 
 def main():
+
+    from urllib.request import Request, urlopen
+            #import lxml
+    import pandas as pd
+    import pandas as pd
+    import time
+    from datetime import datetime, timedelta
+    from selenium import webdriver
+    from bs4 import BeautifulSoup
+    from selenium.webdriver.support.select import Select
+
+
+    
     URL = 'https://redemet.decea.mil.br/old/modal/consulta-de-mensagens/'
-    XPATH = "//*[@class='ui-mainview-block eventpath-wrapper']"
+    datai = "13/02/2024"
+    dataf = "13/02/2024"
+    datahi = datetime.strftime(datai, '%d/%m/%Y %H:%M')
+    datahf = datetime.strftime(dataf, '%d/%m/%Y %H:%M')
+    tempo = datahf - datahi
+    
     TIMEOUT = 20
     
     st.title("Test Selenium")
@@ -45,25 +63,95 @@ def main():
     firefoxOptions = Options()
     firefoxOptions.add_argument("--headless")
     service = Service(GeckoDriverManager().install())
-    driver = webdriver.Firefox(
+    browser = webdriver.Firefox(
         options=firefoxOptions,
         service=service,
     )
-    driver.get(URL)
+    if datahi == datahf:
+        intervalo = 0
+    else:
+        intervalo = int((str(tempo)[0:2]))
+    mes = datahini.month
+
+    
     
     try:
-        WebDriverWait(driver, TIMEOUT).until(
-            EC.visibility_of_element_located((By.XPATH, XPATH,))
-        )
+        for i in range(intervalo + 1):
+                
+            browser = webdriver.Firefox(executable_path='geckodriver.exe')
+               
+            browser.get('https://redemet.decea.mil.br/old/modal/consulta-de-mensagens/')
+            browser.get('https://redemet.decea.mil.br/old/modal/consulta-de-mensagens/')
+              
+            datacori = datahini + timedelta(days=i)
+                   
+            datacoris = datetime.strftime(datacori, '%d/%m/%Y %H:%M')
+            datacori = datetime.strftime(datacori, '%d/%m/%Y %H:%M')
+            datacorf=  datahfim + timedelta(hours=23)
+            datacorfs = datetime.strftime(datacorf, '%d/%m/%Y %H:%M')
+            datacorfs =datacorfs[0:10] + ' 23:00'
+            time.sleep(5)
+     # tira a checkbox para mensagem recente
+            browser.find_element_by_id("consulta_recente").click()
+
+                # preenche o nome das estações para consulta
+            element = browser.find_element_by_id("msg_localidade")
+            element.send_keys(nome)
+
+                # preenche a data inicial e final
+
+            element = browser.find_element_by_id("consulta_data_ini").clear()
+            element = browser.find_element_by_id("consulta_data_ini").click()
+            element = browser.find_element_by_id("consulta_data_ini").send_keys(datacoris)
+            element = browser.find_element_by_id("consulta_data_fim").clear()
+            element = browser.find_element_by_id("consulta_data_fim").click()
+            element = browser.find_element_by_id("consulta_data_fim").send_keys(datacorfs)
+            # envia a consulta
+            botao = browser.find_element_by_id("consulta_localidade")
+            time.sleep(20)
+            botao.click()
+
+                # espera 10s
+            time.sleep(20)
+
+                ## coloca todo o resultado numa página
+                # select_fr = Select(browser.find_element_by_name("msg_resultado_length"))
+                # select_fr.select_by_index(3)
+
+            table = browser.find_element_by_id('msg_resultado')
+
+                # df = pd.read_html(str(table))
+                # print(table)
+            table_html = table.get_attribute('outerHTML')
+
+
+
+
+            if i == 0:
+                df = pd.read_html(str(table_html))
+                df = df[0]
+            else:
+                df2 = pd.read_html(str(table_html))
+                df2 = df2[0]
+
+                df = df.append(df2, ignore_index=True)
+                    
+
+                # print(df.loc[(df["Localidade"] == 'SBSC')])
+            df.to_csv("metar.csv", header=True)
+            browser.quit()
+            
+            # df = df.drop(columns=['Unnamed: 0'])
+        df.to_csv("metar.csv", header=True)
     
     except TimeoutException:
         st.warning("Timed out waiting for page to load")
-        driver.quit()
+        browser.quit()
     
     time.sleep(10)
     elements = driver.find_elements_by_xpath(XPATH)
     st.write([el.text for el in elements])
-    driver.quit()
+    browser.quit()
 
 
 
