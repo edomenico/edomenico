be7ae5021a8b516f5457ca5b895a1eedee386ec2
edomenico <67431641+edomenico@users.noreply.github.com>
2024-02-14 18:52:15 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ data = requests.get(url, headers={'Authorization': f'token ghp_Uvt8k3NseAyt7kZ8t
 
     
     
-with open('metar_trat_teste2.csv', 'wb') as f: 
+with open('metar_trat_teste2.csv', 'w') as f: 
        
     f.write(data.content)
 dff = pd.read_csv('metar_trat_teste2.csv')
