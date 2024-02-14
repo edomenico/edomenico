ef30a3aa90c651578ad4cdc0645db8063d2aa014
edomenico <67431641+edomenico@users.noreply.github.com>
2024-02-14 19:46:22 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ import requests
     
 url = f'https://api.github.com/repos/edomenico/edomenico/contents/metar_trat_teste2.csv?ref=main'
 data = requests.get(url, headers={'Authorization': f'token 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', 'Accept': 'application/vnd.github.v3.raw'})    
-#url = f'https://api.github.com/repos/edomenico/edomenico/contents/metar_trat_teste2.csv?ref=main'
+#url = f'https://api.github.com/repos/edomenico/edomenico/contents/escala1050.xlsx?ref=main'
 #data = requests.get(url, headers='Authorization': f'token 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', 'Accept': 'application/vnd.github.v3.raw')
 
     
     
-with open('metar_trat_teste2.csv', 'a') as f: 
+with open('escala1050.xlsx', 'wb') as f: 
        
     f.write(data.content)
-dff = pd.read_csv('metar_trat_teste2.csv')
+dff = pd.read_csv('escala1050.xlsx')
 print(dff)
 
 
