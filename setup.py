f7b6afcb159d97f709307987c697b5d494c3d2b9
edomenico <67431641+edomenico@users.noreply.github.com>
2024-02-14 09:19:04 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def salva():
     import pandas as pd
     url = f'https://api.github.com/repos/edomenico/edomenico/contents/metar_trat_teste2.csv?ref=main'
     data = requests.get(url, headers={'Authorization': f'token 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', 'Accept': 'application/vnd.github.v3.raw'})
-    with open("metar_trat_teste2.csv", 'w') as f:  
+    with open("metar_trat_teste2.csv", 'a') as f:  
         f.write(data.content)
     #df = pd.read_csv('metar_trat_teste2.csv')
     #print(df)
