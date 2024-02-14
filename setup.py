35cbe4136ef0043f61b8a302fb5cf00478971b1f
edomenico <67431641+edomenico@users.noreply.github.com>
2024-02-14 19:10:28 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ import pandas as pd
 import requests
     
     
-url = f'https://api.github.com/repos/edomenico/edomenico/contents/metar_trat_teste2.csv?ref=main'
-data = requests.get(url, headers={'Authorization': f'token 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', 'Accept': 'application/vnd.github.v3.raw'})    
-
+#url = f'https://api.github.com/repos/edomenico/edomenico/contents/metar_trat_teste2.csv?ref=main'
+#data = requests.get(url, headers={'Authorization': f'token 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', 'Accept': 'application/vnd.github.v3.raw'})    
+url = f'https://api.github.com/repos/{edomenico}/{edomenico}/contents/{metar_trat_teste2.csv}?ref={main}'
+data = requests.get(url, headers={'Authorization': f'token {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}', 'Accept': 'application/vnd.github.v3.raw'})
 
     
     
