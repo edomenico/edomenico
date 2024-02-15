190ca0ba9e26239e1cc8cdceaae0053f62f70440
edomenico <67431641+edomenico@users.noreply.github.com>
2024-02-15 17:16:18 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,10 +10,10 @@
 #repo.create_file('data/dataset.csv', 'upload csv', data, branch='main')
 import pandas as pd
 import requests
-url = 'https://api.github.com/repos/edomenico/edomenico/contents/escala1050.xlsx?ref=main'
+url = 'https://api.github.com/repos/edomenico/edomenico/contents/metar_trat_teste2.csv?ref=main'
 data = requests.get(url, headers={'Authorization': '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', 'Accept': 'application/vnd.github.v3.raw'})
-with open("escala1050.xlsx", 'wb') as f:
+with open("metar_trat_teste2.csv", 'r') as f:
   f.write(data.content)
-df = pd.read_excel('escala1050.xlsx')
+df = pd.read_csv('metar_trat_teste2.csv')
 print(df)
 
