3effc00cd96a67aa2a959dafc1e4df09715f49c4
edomenico <67431641+edomenico@users.noreply.github.com>
2024-02-15 18:29:00 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@
 #repo.create_file('data/dataset.csv', 'upload csv', data, branch='main')
 import pandas as pd
 import requests
-url = 'https://api.github.com/repos/edomenico/edomenico/contents/'metar_trat_teste1.csv?ref=main'
+url = 'https://api.github.com/repos/edomenico/edomenico/contents/metar_trat_teste1.csv?ref=main'
 data = requests.get(url, headers={'Authorization': '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', 'Accept': 'application/vnd.github.v3.raw'})
 with open("'metar_trat_teste1.csv", 'r') as f:
   f.write(data.content)
