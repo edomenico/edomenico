eb705f6e7a49a5307876d6070f1c83d330ba0bbf
edomenico <67431641+edomenico@users.noreply.github.com>
2024-02-15 18:33:03 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ import pandas as pd
 import requests
 url = 'https://api.github.com/repos/edomenico/edomenico/contents/metar_trat_teste1.csv?ref=main'
 data = requests.get(url, headers={'Authorization': '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', 'Accept': 'application/vnd.github.v3.raw'})
-with open("metar_trat_teste1.csv", 'rb') as f:
+with open("metar_trat_teste1.csv", 'r') as f:
   f.write(data.content)
 df = pd.read_csv(f)
 print(df)
