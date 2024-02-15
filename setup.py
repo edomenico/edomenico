61cf05d370f46e6c79092a34389bb777e17cec3e
edomenico <67431641+edomenico@users.noreply.github.com>
2024-02-15 17:04:31 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
-from github import Github
+#from github import Github
 
-g = Github('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')
+#g = Github('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')
 
-repo = g.get_repo('edomenico/edomenico')
+#repo = g.get_repo('edomenico/edomenico')
 
-with open('metar_trat_teste2.csv', 'r') as file:
-    data = file.read()
+#with open('metar_trat_teste2.csv', 'r') as file:
+#    data = file.read()
 
-repo.create_file('data/dataset.csv', 'upload csv', data, branch='main')
-
+#repo.create_file('data/dataset.csv', 'upload csv', data, branch='main')
+import pandas as pd
+import requests
+url = 'https://api.github.com/repos/edomenico/edomenico/contents/escala1050.xlsx?ref=main'
+data = requests.get(url, headers={'Authorization': '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', 'Accept': 'application/vnd.github.v3.raw'})
+with open("escala1050.xlsx", 'wb') as f:
+  f.write(data.content)
+df = pd.read_excel('escala1050.xlsx')
+print(df)
 
