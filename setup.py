ae3528d6bdd182ff125e330ebf04ab1ddaef0004
edomenico <67431641+edomenico@users.noreply.github.com>
2024-02-18 18:26:42 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,114 +1,138 @@
-import streamlit as st
-import requests
+import re
 from bs4 import BeautifulSoup
+from selenium import webdriver
 import pandas as pd
-from streamlit import components
-import os
-import glob
 
-from sites import *
+# Set up the Chrome driver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-
-VALID_USERNAME_PASSWORD_PAIRS = {
-    "edu": "edu"
-}
-
-def salva():
-    from github import Github
-    g = Github("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")
-    repo = g.get_repo("edomenico/edomenico")
-
-    #contents = repo.get_contents("escala1050.xlsx")
-    contents = repo.get_contents("metar_trat_teste2.csv")
-    #repo.delete_file(contents.path,sha=contents, branch="main")
-    #repo.delete_file(contents.path,"remove teste",contents.sha,branch="main")
-
-    repo.update_file(contents.path, "more tests", "more tests", contents.sha, branch="main")
-
-
-
-# Define a function to create the search page
-def create_search_page():
-    # Create a search page with a title and a search bar
-    st.title("Product Price Comparison")
-    p=salva()
-    search_term = st.text_input('Entre com o produto:')
+    def redemet_baixa(escolha, ar, datahini, datahfim,estacao1):
+        # Create Chrome options
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
     
-
-    # Add a button to start scraping the prices
-    if st.button("Search"):
-        with st.spinner('Fetching data. Please wait...'):
-            # Define the URLs for Amazon, Best Buy, and Walmart
-            daraz_df = daraz("https://www.daraz.lk/catalog/?q=" + search_term.replace(" ", "+"))
-            kapruka_df = kapruka("https://www.kapruka.com/srilanka_online_shopping.jsp?d=" + search_term.replace(" ", "%20"))
-            # wasi_df = wasi("https://www.wasi.lk/?s=" + search_term.replace(" ", "+") + "&post_type=product")
-
-            # Convert the "Image Link" column to HTML with image tags
-            def convert_images():
-                daraz_df["Image Link"] = daraz_df["Image Link"].apply(
-                    lambda x: '<img src="' + x + '" width="60" >')
-                kapruka_df["Image Link"] = kapruka_df["Image Link"].apply(
-                   lambda x: '<img src="' + x + '" width="60" >')
-                # wasi_df["Image Link"] = wasi_df["Image Link"].apply(
-                #    lambda x: '<img src="' + x + '" width="60" >')
-
-            def merge_dataframes():
-                # Merge the dataframes vertically
-                merged_df = pd.concat([daraz_df, kapruka_df], axis=0)
-                return merged_df
-
-            convert_images()
-            merged_df = merge_dataframes()
-
-            html = merged_df.to_html(escape=False)
-            #os.chdir("/mount/src/edomenico/metar.csv")
-            #merged_df.to_csv("/mount/src/edomenico/metar.csv", header=True)
-            # Display the prices in a table
-
-            st.markdown(
-                html,
-                unsafe_allow_html=True
-            )
-
-
-# Get the session state for the login status
-session_state = st.session_state
-if "logged_in" not in session_state:
-    session_state["logged_in"] = False
-
-# Create a sidebar for the login page
-st.sidebar.title("Login")
-
-# Add a text input for the username
-username = st.sidebar.text_input("Username")
-
-# Add a text input for the password
-password = st.sidebar.text_input("Password", type="password")
-
-# Add a login button
-if st.sidebar.button("Login"):
-    # Check if the username and password are valid
-    if (username in VALID_USERNAME_PASSWORD_PAIRS) and (password == VALID_USERNAME_PASSWORD_PAIRS[username]):
-        session_state["logged_in"] = True
-        #st.sidebar.success("Logged in!")
-       # p = salva()
-        #st.sidebar.success(print(p))
-        
-        
-        
-    else:
-        st.sidebar.error("Invalid username or password")
-
-# Only create the search page if the user is logged in
-if session_state["logged_in"]:
-    create_search_page()
+        # Create the driver with the options
+        browser = webdriver.Chrome(options=chrome_options)
     
-else:
-    st.warning("Please login to use the search feature")
+        # Load the page with Selenium
+        #browser.get(url)
+    
+        # Wait up to 10 seconds for the page to load
+        # Wait for the page to finish loading all JavaScript
+        wait = WebDriverWait(driver, 10)
 
-####if __name__ == '__main__':
+
+        
+        from datetime import datetime, timedelta
+        import datetime
+        import time
+        if escolha == 1:
+            from urllib.request import Request, urlopen
+            #import lxml
+            import pandas as pd
+            import pandas as pd
+            import time
+            from datetime import datetime, timedelta
+            from selenium import webdriver
+            from bs4 import BeautifulSoup
+            from selenium.webdriver.support.select import Select
+
+            # datai = "01/06/2020 00:00"
+            # dataf = "02/06/2020 23:00"
+            datahi = datetime.strftime(datahini, '%d/%m/%Y %H:%M')
+            datahf = datetime.strftime(datahfim, '%d/%m/%Y %H:%M')
+            tempo = datahfim - datahini
+
+            if datahi == datahf:
+                intervalo = 0
+            else:
+                intervalo = int((str(tempo)[0:2]))
+            mes = datahini.month
+            # nome = "SBSC,SBAR,SBBH,SBBR,SBBV,SBCB,SBCG,SBCP,SBCT,SBCY,SBEN,SBES,SBFL,SBFN,SBFS,SBFZ,SBGL,SBGO,SBGR,SBIL,SBJF,SBJP,SBLB,SBME,SBMM,SBMO,SBMQ,SBNF,SBNT,SBPA,SBPJ,SBPV,SBRB,SBRF,SBRJ,SBSL,SBSP,SBST,SBSV,SBTE,SBMN,SBBE,SBVT,SBSG"
+            nome = estacao1
+            for i in range(intervalo + 1):
+                # abre o Firefox
+                #browser = webdriver.Firefox(executable_path='geckodriver.exe')
+                # browser=webbrowser.open('https://redemet.decea.gov.br/?i=produtos&p=consulta-de-mensagens-opmet', new=2)
+                # browser = webdriver.Chrome(executable_path='chrome.EXE')
+                # chama a página da redemet para consulta
+
+                # browser.get('https://redemet.decea.gov.br/?i=produtos&p=consulta-de-mensagens-opmet')
+                browser.get('https://redemet.decea.mil.br/old/modal/consulta-de-mensagens/')
+                browser.get('https://redemet.decea.mil.br/old/modal/consulta-de-mensagens/')
+                # browser.get('https://www.redemet.aer.mil.br/old/?i=produtos&p=consulta-de-mensagens-opmet')
+                # if (datahi.day + i)==31:
+                datacori = datahini + timedelta(days=i)
+                    # datacori=datahf
+                datacoris = datetime.strftime(datacori, '%d/%m/%Y %H:%M')
+
+
+                datacori = datetime.strftime(datacori, '%d/%m/%Y %H:%M')
+                datacorf=  datahfim + timedelta(hours=23)
+                datacorfs = datetime.strftime(datacorf, '%d/%m/%Y %H:%M')
+                datacorfs =datacorfs[0:10] + ' 23:00'
+
+                # espera 5s
+                time.sleep(5)
+                # tira a checkbox para mensagem recente
+                browser.find_element_by_id("consulta_recente").click()
+
+                # preenche o nome das estações para consulta
+                element = browser.find_element_by_id("msg_localidade")
+                element.send_keys(nome)
+
+                # preenche a data inicial e final
+
+                element = browser.find_element_by_id("consulta_data_ini").clear()
+                element = browser.find_element_by_id("consulta_data_ini").click()
+                element = browser.find_element_by_id("consulta_data_ini").send_keys(datacoris)
+                element = browser.find_element_by_id("consulta_data_fim").clear()
+                element = browser.find_element_by_id("consulta_data_fim").click()
+                element = browser.find_element_by_id("consulta_data_fim").send_keys(datacorfs)
+
+                # envia a consulta
+                botao = browser.find_element_by_id("consulta_localidade")
+                time.sleep(20)
+                botao.click()
+
+                # espera 10s
+                time.sleep(20)
+
+                ## coloca todo o resultado numa página
+                # select_fr = Select(browser.find_element_by_name("msg_resultado_length"))
+                # select_fr.select_by_index(3)
+
+                table = browser.find_element_by_id('msg_resultado')
+
+                # df = pd.read_html(str(table))
+                # print(table)
+                table_html = table.get_attribute('outerHTML')
+                # print(df[0])
+
+                # print(table)
+                if i == 0:
+                    df = pd.read_html(str(table_html))
+                    df = df[0]
+                else:
+                    df2 = pd.read_html(str(table_html))
+                    df2 = df2[0]
+
+                    df = df.append(df2, ignore_index=True)
+                    print(df)
+
+                # print(df.loc[(df["Localidade"] == 'SBSC')])
+                df.to_csv("metar.csv", header=True)
+                browser.quit()
+            print(df)
+            # df = df.drop(columns=['Unnamed: 0'])
+            df.to_csv("metar.csv", header=True)
+            # df.to_csv('example.csv')
+if __name__ == '__main__':
     #if streamlit._is_running_with_streamlit:
-    ####main()
+    main()
     #else:
     #    sys.argv = ["streamlit", "run", sys.argv[0]]
     #    #app.run_server(debug=True, port=8881)
