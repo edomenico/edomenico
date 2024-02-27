9f5411c4ee8aaf241aa0d992fc2e97d820e589e9
edomenico <67431641+edomenico@users.noreply.github.com>
2024-02-27 18:18:37 -0300

    Update tabuleiro.py

--- tabuleiro.py
+++ tabuleiro.py
@@ -391,6 +391,7 @@ def main():
                 nome_estacao = noestacao[i]
                 arqi = arqi1.loc[(arqi1['estacao'] == nome_estacao)]
                 arqi = arqi.reset_index(drop=True)
+                print (arqi)
                 #cwd = os.getcwd()
                 #if areatrab == 1:
 
