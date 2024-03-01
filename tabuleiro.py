3df767cb8366e3be1481e858454a87bc57353fa5
edomenico <67431641+edomenico@users.noreply.github.com>
2024-03-01 17:27:09 -0300

    Update tabuleiro.py

--- tabuleiro.py
+++ tabuleiro.py
@@ -1147,17 +1147,21 @@ def main():
         datainicio = datainicio.strftime('%d/%m/%y')
 
         if areatrab == 1:
+            
           #  estacao_area = 'SBJR,SBES,SBME,SBCP,SBFS,SBRJ,SBCB,SBVT,SBPS,SBGL,SBNT,SBMS,SBAC,SBJE,SBPB,SBAR,SBMO,SBRF,SBJP,SBSG,SBFZ,SBSL,SBTE,SBJU,SBKG,SBFN,SBPL,SBPJ'
             # estacao_area = 'SBFZ,'
             #arqi1 = pd.read_csv('metar_trat_teste1.csv')
             arqi1 = pd.read_csv('metar_trat_teste1.csv')
         else:
-
+            if dados2==0:
             #estacao_area = 'SBRD,SBVH,SWEI,SBJI,SBRB,SSKW,SBCY,SBPV,SBCZ,SBTT,SBIZ,SBCI,SBMA,SBCJ,SBHT,SBTB,SBOI,SBBE,SBMQ,SBSN,SBSO,SBSI,SBAT,SBIH,SBMY,SWPI,SBTF,SBUA,SBEG,SBBV'  # sem SBMY SBCY
             # estacao_area = 'SBVH'
             # estacao_area ='SBEG,'
             #arqi1 = pd.read_csv('metar_trat_teste2.csv')
-            arqi1 = pt2
+                arqi1 = pt2
+            else:
+                arqi1 = pd.read_csv('metar_trat_teste2.csv')
+                
         estacao_area=est
         noestacao = estacao_area.split(',')
 
