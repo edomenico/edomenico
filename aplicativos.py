cf9f0e902e1abfe14a34beee3445b53098e7f89c
edomenico <67431641+edomenico@users.noreply.github.com>
2024-03-08 19:53:15 -0300

    Update aplicativos.py

--- aplicativos.py
+++ aplicativos.py
@@ -2324,6 +2324,12 @@ def main2():
             start_datee = datetime.today()
             nome_estacao='N'
             with st.sidebar:
+                pt1 = pd.read_csv('/mount/src/edomenico/metar_trat_teste1.csv',
+                                    sep=',',
+                                    decimal='.')
+                pt2 = pd.read_csv('/mount/src/edomenico/metar_trat_teste2.csv',
+                                    sep=',',
+                                    decimal='.')
                 with st.container(border=True):
                     on = st.toggle('Atualizar dados')
                     if on:
