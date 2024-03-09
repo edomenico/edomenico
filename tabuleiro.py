3dd0bd597902a34c4cb3dff1f1d75b3a0479836b
edomenico <67431641+edomenico@users.noreply.github.com>
2024-03-09 15:23:35 -0300

    Update tabuleiro.py

--- tabuleiro.py
+++ tabuleiro.py
@@ -2385,11 +2385,7 @@ def main():
     #    sys.argv = ["streamlit", "run", sys.argv[0]]
     #    #app.run_server(debug=True, port=8881)
       #  sys.exit(stcli.main())
-st.set_page_config(
-        page_title="Tabuleiro - CMA-GL",
-        page_icon="✅",
-        layout="wide",
-    )
+
 st.session_state
 
 main()
