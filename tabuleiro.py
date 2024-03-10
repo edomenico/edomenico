cbe56bdfe3dc5915158c84573d14e80e4279aa08
edomenico <67431641+edomenico@users.noreply.github.com>
2024-03-10 19:15:53 -0300

    Update tabuleiro.py

--- tabuleiro.py
+++ tabuleiro.py
@@ -2269,7 +2269,7 @@ def main():
                                     sep=',',
                                     decimal='.')
         with st.container(border=True):
-            st.write('Gerenciamento dos dados')
+            st.write('Visualização dos dados')
             on = st.toggle('Atualizar dados das duas áreas')
             if on:    
                # st.write('Atualização dos Dados(das 2 áreas)')
@@ -2306,12 +2306,7 @@ def main():
                     pt = rest(2,to_data,from_data)
         
                     my_bar.progress(100, text="Terminou")
-            #st.divider()
-            
-            
-
-        with st.container(border=True):
-           # st.divider()
+            st.divider()
             on2 = st.toggle('Consultar outro período')
             if on2:
                 selecionaperiodo= st.radio('Escolha o período',['Últimos 10 dias','Selecionar dia inicial (a partir de 01/10/23)'],horizontal=True)
@@ -2323,8 +2318,10 @@ def main():
                    # datainicial= datainicial-timedelta(9)
             else:
                 datainicial = datetime.utcnow() - timedelta(9)
-            #st.divider()
-            
+        #st.divider()
+
+        with st.container(border=True):
+           # st.divider()
             st.write('Visualização dos dados')
             
             
