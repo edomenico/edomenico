75642fc24db9765598a9a226d4f43bc95cbfb91c
edomenico <67431641+edomenico@users.noreply.github.com>
2024-03-11 21:19:30 -0300

    Update tabuleiro.py

--- tabuleiro.py
+++ tabuleiro.py
@@ -1169,7 +1169,7 @@ def main():
                 nome_estacao = noestacao[i]
                 arqi = arqi1.loc[(arqi1['estacao'] == nome_estacao)]
                 arqi = arqi.reset_index(drop=True)
-                print('arquivo: ', arqi)
+               @ print('arquivo: ', arqi)
                
                 #cwd = os.getcwd()
                 #if areatrab == 1:
@@ -1254,7 +1254,7 @@ def main():
                 arqi['aviso'] = stop
                 arqi['nspc'] = noespec
                 
-                print(valores)
+                #print(valores)
                 # arqi['especial'] = arqi['especial'].str[:50]
                 # arqi = arqi.reset_index(drop=True)
 
