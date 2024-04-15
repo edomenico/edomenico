b2c44b9cdd3cf0b618bd5e842e6544becb589f62
edomenico <67431641+edomenico@users.noreply.github.com>
2024-04-15 17:59:01 -0300

    Update baixarmodeloNovoprojeto.py

--- baixarmodeloNovoprojeto.py
+++ baixarmodeloNovoprojeto.py
@@ -83,7 +83,7 @@ def Scraper(estacao):
         chrome_options.add_argument("--headless")
 
         driver = webdriver.Chrome(options=chrome_options)
-
+        print('chegou aqui 5')
         #for no in range(0, len(arqi), 1):
         for jj in range(0,1,1):
             try:
