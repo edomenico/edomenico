d0ce9aa754d3006942a6f908b18118f37c2b8244
edomenico <67431641+edomenico@users.noreply.github.com>
2024-04-16 17:39:50 -0300

    Update backend2.py

--- backend2.py
+++ backend2.py
@@ -450,10 +450,13 @@ def authenticate(city):
     print('chegou aqui 2')
 
 
-    link,data,horazulu=baixarmodeloNovoprojeto.Scraper(city)
+    #link,data,horazulu=baixarmodeloNovoprojeto.Scraper(city)
+    city='SBJR'
+    link='https://www.windy.com/-22.910/-43.163/meteogram?-22.935,-43.163,13,m:c0YaeXe'
+    horazulu=3
     print('chegou aqui 3')
     data1=baixaamodeloNovometeograma.Scraper(city,link,horazulu)
-
+    print('chegou aqui 2222222222222')
    
 
     #except Exception as e:
