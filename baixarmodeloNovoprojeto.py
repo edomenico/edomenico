551b41d64608ef95d31dee185b7c240ed92679f8
edomenico <67431641+edomenico@users.noreply.github.com>
2024-04-16 08:45:55 -0300

    Update baixarmodeloNovoprojeto.py

--- baixarmodeloNovoprojeto.py
+++ baixarmodeloNovoprojeto.py
@@ -18,6 +18,7 @@ import time
 # Set up the Chrome driver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 def inicio():
@@ -78,12 +79,25 @@ def qnuvem(s):
     return nuv,tp,vis,nuvcb
 
 #arqi=pd.read_csv('estacaomodeloicon.csv', encoding='iso-8859-1', delimiter =';')
-
+def get_webdriver_options(proxy: str = None) -> Options:
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-features=NetworkService")
+    options.add_argument("--window-size=1920x1080")
+    options.add_argument("--disable-features=VizDisplayCompositor")
+    options.add_argument('--ignore-certificate-errors')
+    if proxy is not None:
+        options.add_argument(f"--proxy-server=socks5://{proxy}")
+    options.set_capability('goog:loggingPrefs', {'performance': 'ALL'})
+    return options
 
 
 
 #link='https://www.windy.com/-22.910/-43.163/meteogram?-22.935,-43.163,13,m:c0YaeXe' #meteograma
-def Scraper(estacao):
+def Scraper(estacao, proxy: str=None):
 
         
         
@@ -94,9 +108,10 @@ def Scraper(estacao):
         # self.driver.set_window_size(1120, 550)
 
         print('chegou aqui 4444')
-        chrome_options = Options()
+        #chrome_options = Options()
         print('chegou aqui 5')
-        chrome_options.add_argument("--headless")
+        #chrome_options.add_argument("--headless")
+        webdriver.Chrome(options=get_webdriver_options(proxy=proxy)) as driver:
         print('chegou aqui 666')
         driver = webdriver.Chrome(options=chrome_options)
         wait = WebDriverWait(driver, 20)
