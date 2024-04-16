89f31120639f9fa0414d50f25189d4a6dcb2d445
edomenico <67431641+edomenico@users.noreply.github.com>
2024-04-16 17:48:48 -0300

    Update baixaamodeloNovometeograma.py

--- baixaamodeloNovometeograma.py
+++ baixaamodeloNovometeograma.py
@@ -49,13 +49,16 @@ def Scraper(estacao,link,horazulu):
                     print('Loading...')
                     print('chegou aqui b2')
                     driver.get(link1)
+                    print('chegou aqui b3')
 
                     forecast = {}
 
                 # while True:
 
                     sleep(12)
+                    print('chegou aqui b4')
                     s = BeautifulSoup(driver.page_source, "html.parser")
+                    print('chegou aqui b5')
                    # horagmt=arqi['horzulu'][no]
                     # text_file = open("forecast.txt", "w")
                     # text_file.write(s.find_all('script'))
