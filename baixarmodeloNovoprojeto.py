2478f56aba81024a513c8a252ab13edf3712c36d
edomenico <67431641+edomenico@users.noreply.github.com>
2024-04-16 16:27:59 -0300

    Update baixarmodeloNovoprojeto.py

--- baixarmodeloNovoprojeto.py
+++ baixarmodeloNovoprojeto.py
@@ -145,7 +145,7 @@ def Scraper(estacao):
                     print('Loading...')
                     print('chegou aqui 11')
                     print('chegou aqui 11 ',link)
-                    html = requests.get(link)
+                    html = requests.get(link).content
                     if html.status_code in [200]:
                             
                             print('link ok')
