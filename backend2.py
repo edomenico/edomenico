5dd3b2a1897c48f004a39d53bf18a918f6a7e2a4
edomenico <67431641+edomenico@users.noreply.github.com>
2024-04-16 19:57:20 -0300

    Update backend2.py

--- backend2.py
+++ backend2.py
@@ -94,6 +94,7 @@ def authenticate(city):
     #    st.stop()
     #data = pd.read_csv("dadosecmwf_area2_1104.csv")
     data=pd.merge(data, data1, how='inner', on='datahora')
+    print('chegou aqui 44444444444444')
     data=data.drop(['tar_y', 'estacao_y', 'datazulu_y'], 1)
     data=data.rename(columns={'estacao_x': 'estacao', 'tar_x': 'tar', 'datazulu_x': 'datazulu'})
 
