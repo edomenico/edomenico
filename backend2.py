2656c62130033d318c92723d0a4a788d877acada
edomenico <67431641+edomenico@users.noreply.github.com>
2024-04-17 13:15:06 -0300

    Update backend2.py

--- backend2.py
+++ backend2.py
@@ -96,11 +96,12 @@ def authenticate(city):
     data=pd.merge(data, data1, how='inner', on='datahora')
     print(data)
     print('chegou aqui 44444444444444')
-    data1=data.drop(['tar_y', 'estacao_y', 'datazulu_y'], axis=1)
+    #data1=data.drop(['tar_y', 'estacao_y', 'datazulu_y'], axis=1)
     print('chegou aqui 5555555555555')
-    print(data1)
+    
     data=data.rename(columns={'estacao_x': 'estacao', 'tar_x': 'tar', 'datazulu_x': 'datazulu'}, inplace=True)
     print('chegou aqui 66666666666666')
+    print(data)
 
 
     return data
