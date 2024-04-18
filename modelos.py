3b39d6755fadd1f2040cdd2b831531a344817bc3
edomenico <67431641+edomenico@users.noreply.github.com>
2024-04-18 20:04:30 -0300

    Update modelos.py

--- modelos.py
+++ modelos.py
@@ -677,7 +677,7 @@ def main():
         #link,data1,horazulu=baixarmodeloNovoprojeto(city)
         city='SBJR'
         #####horazulu='3'
-        ####link='https://www.windy.com/-22.910/-43.163/meteogram?-22.935,-43.163,13,m:c0YaeXe'
+        link='https://www.windy.com/-22.910/-43.163/meteogram?-22.935,-43.163,13,m:c0YaeXe'
         horazulu='3'
         
         data2=baixaamodeloNovometeograma(city,link,horazulu)
