d4cf09d3aeb1efb90a8beea1fc9f872e573c58d0
edomenico <67431641+edomenico@users.noreply.github.com>
2024-04-18 11:25:59 -0300

    Update modelos.py

--- modelos.py
+++ modelos.py
@@ -219,7 +219,8 @@ def main():
                                                 b = re.sub('[^0-9]', '', str(rows[i].contents[j]))
                                                 #if taraux.isdigit():
                                                     #apenasDigitos = apenasDigitos + taraux
-                                            tar.append(str(b))
+                                            bb= (b - 32) * 5/9
+                                            tar.append(str(bb))
                                             
                                             print(str(b))
                                             print('chegou aqui 8')
