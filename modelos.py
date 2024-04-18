41892b7ec48b1559a83761e080919bfb0469a742
edomenico <67431641+edomenico@users.noreply.github.com>
2024-04-18 14:08:07 -0300

    Update modelos.py

--- modelos.py
+++ modelos.py
@@ -264,7 +264,7 @@ def main():
                         #for i in range(0,len(estacao),1):
                         if hhh='0':
                             hhh='0'+hhh
-                        datahora1=ddd +" "+hhh+':00"
+                        datahora1=ddd +" "+hhh"+":00"
                         print('chegou aqui 7')
                         print(datahora1)
                         
