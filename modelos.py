0f0e60b6d5e49cd5efdfd8acfc7bbf17132aac19
edomenico <67431641+edomenico@users.noreply.github.com>
2024-04-19 10:02:56 -0300

    Update modelos.py

--- modelos.py
+++ modelos.py
@@ -192,7 +192,7 @@ def main():
                                         dataaux.append(datetime.now() + timedelta(days=j))
                                         data.append(rows[i].contents[j].string)
                                         print('baixarmodelo 1111')
-                                        print(rows[i].contents[j].string)
+                                        print(rows[1].contents[0])
                                         
                                         hhh=(str(rows[1].contents[0])[28:29])
                                         hhhh=(str(rows[1].contents[0])[36:38])
