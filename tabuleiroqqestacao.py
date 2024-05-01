2427f80ebf1d741a2f3366d35411e473f97db92a
edomenico <67431641+edomenico@users.noreply.github.com>
2024-05-01 19:55:27 -0300

    Update tabuleiroqqestacao.py

--- tabuleiroqqestacao.py
+++ tabuleiroqqestacao.py
@@ -99,7 +99,7 @@ def main2():
 
         global pt
 
-        def trata_redemet(areasele):
+        def trata_redemet(areasele,estacao):
             
             import matplotlib.pyplot as plt
             import matplotlib.dates as md
@@ -226,11 +226,13 @@ def main2():
 
                         # campo 3 vento
                         # arqi.Mensagem[i][arqi.Mensagem[i].find('KT')
-                        if mensagem1[j].find('SB') > -1 or mensagem1[j].find('SSKW') > -1 or mensagem1[j].find(
-                                'SWPI') > -1 or mensagem1[j].find('SWEI') > -1 or mensagem1[j].find('SWLC') > -1\
-                                or mensagem1[j].find('SNRU') > -1 or mensagem1[j].find('SNGI') > -1 or mensagem1[j].find('SNTF') > -1\
-                                or mensagem1[j].find('SDYH') > -1 or mensagem1[j].find('SNVB') > -1:
-                            estacao.append(mensagem1[j])
+                        if j==1:
+                            if mensagem1[j].find('SB') > -1 or mensagem1[j].find('SSKW') > -1 or mensagem1[j].find(
+                                    'SWPI') > -1 or mensagem1[j].find('SWEI') > -1 or mensagem1[j].find('SWLC') > -1 \
+                                    or mensagem1[j].find('SNRU') > -1 or mensagem1[j].find(estacao) > -1 or mensagem1[
+                                j].find('SNTF') > -1 \
+                                    or mensagem1[j].find('SDYH') > -1 or mensagem1[j].find('SNVB') > -1:
+                                estacao.append(mensagem1[j])
                         # if estacao[k] == 'SBFZ':
                         #      GGGGGGGGG = 1
 
@@ -643,7 +645,7 @@ def main2():
             areaprev = 2
         pdf = redemet_baixa2(1, areasel, to_data, from_data, estacao)
 
-        pdff = trata_redemet(areaprev)
+        pdff = trata_redemet(areaprev,estacao)
        # edited_df = st.data_editor(pdff)
         return pdff
 
@@ -826,21 +828,14 @@ def main2():
                         b = 'METAR'
                     else:
                         b = 'SPECI'
-                    if p[i].find('SB') > -1:
-                        a = p[i][p[i].find('SB'):p[i].find('SB') + 4]  # or p[i].find('SSKW') > -1 or mensagem1[i].find('SWPI') > -1 or mensagem1[i].find('SWEI') > -1:
-                    elif p[i].find('SSKW') > -1:
-                        a = 'SSKW'
-                    elif p[i].find('SWPI') > -1:
-                        a = 'SWPI'
-                    elif p[i].find('SWEI') > -1:
-                        a = 'SWEI'
-                    elif p[i].find('SNRU') > -1:
-                        a = 'SNRU'
+                    if p[i].find(estacao1) > -1:
+                        a = estacao1 # or p[i].find('SSKW') > -1 or mensagem1[i].find('SWPI') > -1 or mensagem1[i].find('SWEI') > -1:
+                    
 
                     montalinha = str(i) + ',' + a + ',' + b + ',' + datainicio + ',' + p[i][0:p[i].find('=') + 1]
                     arquivo.append(montalinha)
                     f.write("\n" + montalinha)
-            f.close
+            #f.close
             return arquivo
 
 
