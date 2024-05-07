fb30fb22d2dc0a2c7b15cf3bebc42ecb897b24e0
edomenico <67431641+edomenico@users.noreply.github.com>
2024-05-07 19:12:35 -0300

    Update tabuleiroqqestacao.py

--- tabuleiroqqestacao.py
+++ tabuleiroqqestacao.py
@@ -2660,7 +2660,10 @@ def main2():
                     st.divider()
                     with st.expander(label="Mostrar dados:"):
                         df1=df
-                        
+                        df1.drop('data_hora', inplace=True, axis=1)
+                        df1.drop('drytt', inplace=True, axis=1)
+                        df1.drop('dewpt', inplace=True, axis=1)
+                        st.table(df1)
                 
 
             
