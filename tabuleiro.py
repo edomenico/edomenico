d8bdd1132dc0a321c75ba31258b6c208e9fb42f0
edomenico <67431641+edomenico@users.noreply.github.com>
2024-11-28 12:11:28 -0300

    Update tabuleiro.py

--- tabuleiro.py
+++ tabuleiro.py
@@ -2559,7 +2559,7 @@ def main():
             on2 = st.toggle('Consultar outro período')
             
             if on2:
-                selecionaperiodo= st.radio('Escolha o período',['Últimos 10 dias','Selecionar dia inicial (a partir de 01/09/24)'],horizontal=True)
+                selecionaperiodo= st.radio('Escolha o período',['Últimos 10 dias','Selecionar dia inicial (a partir de 01/11/24)'],horizontal=True)
                 if selecionaperiodo=='Últimos 10 dias':
                     datainicial = datetime.utcnow() - timedelta(9)
                 else:
