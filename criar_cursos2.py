cba32bd239cb6b7d4bd67814d2fd45e23a91b3c4
edomenico <67431641+edomenico@users.noreply.github.com>
2024-12-20 11:11:22 -0300

    Update criar_cursos2.py

--- criar_cursos2.py
+++ criar_cursos2.py
@@ -383,7 +383,7 @@ def form_curso(estarea, horainicio, dataenvio, diainicio, estacao):
         with st.container(border=True):
             st.markdown("## :gear: Grupo de Mudanças")
             options = ["BECMG", "FM", "PROB30", "PROB40", "TEMPO", "PROB30 TEMPO", "PROB40 TEMPO"]
-            selection = st.radio("Mudanças", options)
+            selection = st.radio("Mudanças", options,horizontal=True)
 
             # if selection:
             if selection == "BECMG":
