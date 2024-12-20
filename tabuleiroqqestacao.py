0e4b7905b7d773ffa7fc6d686765bdecd90d4f49
edomenico <67431641+edomenico@users.noreply.github.com>
2024-12-20 14:11:34 -0300

    Update tabuleiroqqestacao.py

--- tabuleiroqqestacao.py
+++ tabuleiroqqestacao.py
@@ -1964,8 +1964,7 @@ def main2():
                 arqi['tp'] = arqi['tp'].replace(['+SHRA'], "+" + m)
                 arqi['tp'] = arqi['tp'].replace(['SHRA'], m)
                 arqi['tp'] = arqi['tp'].replace(['+TSGRRA'],"+" + o)
-                arqi['tp'] = arqi['tp'].replace(['TSGRRA'],o)
-                arqi['tp'] = arqi['tp'].replace(['-TSGRRA'],"+" - o)
+               
 
                 arqi['aviso'] = arqi['aviso'].replace(['A'], n)
                 # fim
