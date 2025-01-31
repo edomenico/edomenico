4eb359aa34b1484208557b0a637eff6688679d36
edomenico <67431641+edomenico@users.noreply.github.com>
2025-01-31 14:08:06 -0300

    Update tabuleiroqqestacao.py

--- tabuleiroqqestacao.py
+++ tabuleiroqqestacao.py
@@ -1763,6 +1763,7 @@ def main2():
                         ano_fim = str(date_fim.year)
                 else:
                     dia_fim = str(date_fim.day - 1)
+                    dia_fim = '31'
                     #dia_fim = str(date_fim.day)
                     ano_fim = str(date_fim.year)
 
