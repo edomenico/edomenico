83cc05b51cbdd74aca1ec3be4ae435f37c87c098
edomenico <67431641+edomenico@users.noreply.github.com>
2025-03-02 23:01:41 -0300

    Update tabuleiro.py

--- tabuleiro.py
+++ tabuleiro.py
@@ -1283,7 +1283,7 @@ def main():
                 if mesini == 2:
                     if (date.today().year % 4) != 0:
                         p.xaxis.formatter = FuncTickFormatter(code="""
-                                               var mapping = {1: 28, 2: 1, 2: 2, 3: 3, 4:4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9};
+                                               var mapping = {1: 28, 2: 1, 3: 2, 4: 3, 5:4, 6: 5, 7: 6, 8: 7, 9: 8, 10: 9};
                                                return mapping[tick];
                                           """)
                     else:
