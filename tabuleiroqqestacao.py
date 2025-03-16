cf929884642a87201318d1832582a1cc8aaa4c72
edomenico <67431641+edomenico@users.noreply.github.com>
2025-03-16 19:44:54 -0300

    Update tabuleiroqqestacao.py

--- tabuleiroqqestacao.py
+++ tabuleiroqqestacao.py
@@ -767,7 +767,7 @@ def main2():
 
                         if mensagem1[j].find('FEW') > -1 or mensagem1[j].find('SCT') > -1 or mensagem1[j].find(
                                 'BKN') > -1 or \
-                                mensagem1[j].find('OVC') > -1 or mensagem1[j].find('VV'):
+                                mensagem1[j].find('OVC') > -1 :
                             if mensagem1[j].find('CB') < 0 and mensagem1[j].find('TCU') < 0:
                                 if camada1 == False:
                                     if (mensagem1[j][3:6]).isnumeric():
@@ -852,7 +852,7 @@ def main2():
                             altn2.append('NaN')
                             altn3.append('NaN')
                             altn4.append('NaN')
-                            qn1.append('VVv')
+                            qn1.append('OVC')
                             qn2.append('NaN')
                             qn3.append('NaN')
                             qn4.append('NaN')
@@ -867,7 +867,7 @@ def main2():
                                     'FU') > -1 or mensagem1[j].find('VC') > -1 or
                             mensagem1[j].find('TS') > -1) and mensagem1[j].find('SB') == -1 and mensagem1[
                             j].find(
-                            'VV') == -1 and tempopres == False:
+                            'OVC') == -1 and tempopres == False:
                             tp.append((mensagem1[j][0:len(mensagem1[j])]))
                             tempopres = True
 
