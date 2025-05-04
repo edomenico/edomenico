d195430b5c358f82bb3c5e4f78537966b80fa528
edomenico <67431641+edomenico@users.noreply.github.com>
2025-05-04 14:19:46 -0300

    Update modelos.py

--- modelos.py
+++ modelos.py
@@ -106,7 +106,7 @@ def main():
     
             chrome_options = Options()
             chrome_options.add_argument("--headless")
-            print(foi aqui)
+            print('foi aqui')
                 # Create the driver with the options
             driver = webdriver.Chrome(options=chrome_options)
             driver.implicitly_wait(120)
