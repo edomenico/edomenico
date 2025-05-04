4b61da763d5892c75a7474a16965007b2d331139
edomenico <67431641+edomenico@users.noreply.github.com>
2025-05-04 12:49:35 -0300

    Update modelos.py

--- modelos.py
+++ modelos.py
@@ -73,9 +73,6 @@ def main():
             from bs4 import BeautifulSoup
             from time import sleep
             from datetime import datetime,timedelta
-
-            import chromedriver_autoinstaller
-            from webdriver_manager.chrome import ChromeDriverManager
             from datetime import date
             from pytz import timezone
             from selenium.webdriver.chrome.options import Options
@@ -83,36 +80,36 @@ def main():
             from selenium.webdriver.support.wait import WebDriverWait
             from selenium.webdriver.support import expected_conditions as EC
             
-            #from selenium.webdriver.chrome.service import Service
-            #from webdriver_manager.chrome import ChromeDriverManager
+            from selenium.webdriver.chrome.service import Service
+            from webdriver_manager.chrome import ChromeDriverManager
             
-            # from selenium.common.exceptions import TimeoutException
-            # from selenium.webdriver.common.by import By
-            # from selenium.webdriver.firefox.options import Options
-            # from selenium.webdriver.firefox.service import Service
-            # from selenium.webdriver.support import expected_conditions as EC
-            # from selenium.webdriver.support.ui import WebDriverWait
+            from selenium.common.exceptions import TimeoutException
+            from selenium.webdriver.common.by import By
+            from selenium.webdriver.firefox.options import Options
+            from selenium.webdriver.firefox.service import Service
+            from selenium.webdriver.support import expected_conditions as EC
+            from selenium.webdriver.support.ui import WebDriverWait
             #from webdriver_manager.firefox import GeckoDriverManager
     
     
             
-            #chromedriver_autoinstaller.install()
+    
            # driver = webdriver.Firefox()
             # self.driver.set_window_size(1120, 550)
-            #print('baixarmodeloNovoprojeto')
-            #print(estacao)
+            print('baixarmodeloNovoprojeto')
+            print(estacao)
             options = Options()
             options.add_argument('--disable-gpu')
             options.add_argument('--headless')
-            #
-            #
-            #
+        
+    
+    
             chrome_options = Options()
             chrome_options.add_argument("--headless")
             
                 # Create the driver with the options
-            #driver = webdriver.Chrome(options=chrome_options)
-            driver = webdriver.Chrome(ChromeDriverManager().install())
+            driver = webdriver.Chrome(options=chrome_options)
+
             driver.implicitly_wait(20)
            # driver = webdriver.Chrome()
             #firefoxOptions = Options()
@@ -395,45 +392,44 @@ def main():
             from selenium import webdriver
             from bs4 import BeautifulSoup
             from time import sleep
-            from datetime import datetime, timedelta
-
-            import chromedriver_autoinstaller
-            from webdriver_manager.chrome import ChromeDriverManager
+            from datetime import datetime,timedelta
             from datetime import date
             from pytz import timezone
             from selenium.webdriver.chrome.options import Options
             from selenium.webdriver.common.by import By
             from selenium.webdriver.support.wait import WebDriverWait
             from selenium.webdriver.support import expected_conditions as EC
-
-            # from selenium.webdriver.chrome.service import Service
-            # from webdriver_manager.chrome import ChromeDriverManager
-
-            # from selenium.common.exceptions import TimeoutException
-            # from selenium.webdriver.common.by import By
-            # from selenium.webdriver.firefox.options import Options
-            # from selenium.webdriver.firefox.service import Service
-            # from selenium.webdriver.support import expected_conditions as EC
-            # from selenium.webdriver.support.ui import WebDriverWait
-            # from webdriver_manager.firefox import GeckoDriverManager
-
-            # chromedriver_autoinstaller.install()
-            # driver = webdriver.Firefox()
+            
+            from selenium.webdriver.chrome.service import Service
+            from webdriver_manager.chrome import ChromeDriverManager
+            
+            from selenium.common.exceptions import TimeoutException
+            from selenium.webdriver.common.by import By
+            from selenium.webdriver.firefox.options import Options
+            from selenium.webdriver.firefox.service import Service
+            from selenium.webdriver.support import expected_conditions as EC
+            from selenium.webdriver.support.ui import WebDriverWait
+            #from webdriver_manager.firefox import GeckoDriverManager
+    
+    
+            
+    
+           # driver = webdriver.Firefox()
             # self.driver.set_window_size(1120, 550)
-            #print('baixarmodeloNovoprojeto')
-            #print(estacao)
+            print('baixarmodeloNovoprojeto')
+            print(estacao)
             options = Options()
             options.add_argument('--disable-gpu')
             options.add_argument('--headless')
-            #
-            #
-            #
+        
+    
+    
             chrome_options = Options()
             chrome_options.add_argument("--headless")
+            
+                # Create the driver with the options
+            driver = webdriver.Chrome(options=chrome_options)
 
-            # Create the driver with the options
-            # driver = webdriver.Chrome(options=chrome_options)
-            driver = webdriver.Chrome(ChromeDriverManager().install())
             driver.implicitly_wait(20)
             
 
