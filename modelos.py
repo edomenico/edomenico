a1b478ce302de8f61be3d2cabd2bb03c6b844818
edomenico <67431641+edomenico@users.noreply.github.com>
2025-05-07 10:15:30 -0300

    Update modelos.py

--- modelos.py
+++ modelos.py
@@ -85,8 +85,8 @@ def main():
             
             from selenium.common.exceptions import TimeoutException
             from selenium.webdriver.common.by import By
-            from selenium.webdriver.firefox.options import Options
-            from selenium.webdriver.firefox.service import Service
+            #from selenium.webdriver.firefox.options import Options
+            #from selenium.webdriver.firefox.service import Service
             from selenium.webdriver.support import expected_conditions as EC
             from selenium.webdriver.support.ui import WebDriverWait
             #from webdriver_manager.firefox import GeckoDriverManager
@@ -402,11 +402,11 @@ def main():
             
             from selenium.common.exceptions import TimeoutException
             from selenium.webdriver.common.by import By
-            from selenium.webdriver.firefox.options import Options
-            from selenium.webdriver.firefox.service import Service
+            #from selenium.webdriver.firefox.options import Options
+            #from selenium.webdriver.firefox.service import Service
             from selenium.webdriver.support import expected_conditions as EC
             from selenium.webdriver.support.ui import WebDriverWait
-            from webdriver_manager.firefox import GeckoDriverManager
+            #from webdriver_manager.firefox import GeckoDriverManager
             chrome_options = Options()
             chrome_options.add_argument("--headless")
             
