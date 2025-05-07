337984160d88bffb2badeb6e6ab9be31ebb40377
edomenico <67431641+edomenico@users.noreply.github.com>
2025-05-07 09:55:27 -0300

    Update modelos.py

--- modelos.py
+++ modelos.py
@@ -89,26 +89,19 @@ def main():
             from selenium.webdriver.firefox.service import Service
             from selenium.webdriver.support import expected_conditions as EC
             from selenium.webdriver.support.ui import WebDriverWait
-            #from webdriver_manager.firefox import GeckoDriverManager
-    
-    
+            import os, sys
+
+
+            def installff():
+              os.system('sbase install geckodriver')
+              os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
             
-    
-           # driver = webdriver.Firefox()
-            # self.driver.set_window_size(1120, 550)
-            print('baixarmodeloNovoprojeto')
-            print(estacao)
-            options = Options()
-            options.add_argument('--disable-gpu')
-            options.add_argument('--headless')
-        
-    
-    
-            chrome_options = Options()
-            chrome_options.add_argument("--headless")
-            
-                # Create the driver with the options
-            driver = webdriver.Chrome(options=chrome_options)
+            _ = installff()
+            from selenium import webdriver
+            from selenium.webdriver import FirefoxOptions
+            opts = FirefoxOptions()
+            opts.add_argument("--headless")
+            browser = webdriver.Firefox(options=opts)
             #firefoxOptions = Options()
             #firefoxOptions.add_argument("--headless")
             #service = Service(GeckoDriverManager().install())
