c7c11fb56bead45183395ed3ab001913e62209b0
edomenico <67431641+edomenico@users.noreply.github.com>
2025-07-09 15:33:36 -0300

    Update tabuleiro.py

--- tabuleiro.py
+++ tabuleiro.py
@@ -2788,7 +2788,7 @@ def main():
     p=tabuleiro(nomedaestacao,noarea,datainicial)
         
     import streamlit.components.v1 as components
-    from streamlit_bokeh import streamlit_bokeh
+    #from streamlit_bokeh import streamlit_bokeh
     
     #streamlit_bokeh(p)
     st.components.v1.html(p,  height=2400,width=1700, scrolling=True)
