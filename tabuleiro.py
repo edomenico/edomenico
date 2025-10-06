44a9304d1bf781a13ebe56ae418b6acdcc1d863c
edomenico <67431641+edomenico@users.noreply.github.com>
2025-10-06 19:07:42 -0300

    Update tabuleiro.py

--- tabuleiro.py
+++ tabuleiro.py
@@ -2784,13 +2784,7 @@ def main():
     
     import streamlit.components.v1 as components
     # from streamlit_bokeh import streamlit_bokeh
-    while True:
-        # Código para buscar e atualizar dados
-        
-        
-        
-        # Aguarda por uma hora antes de repetir
-        time.sleep(15) # 3600 segundos = 1 hora
+    
     
     
     # streamlit_bokeh(p)
