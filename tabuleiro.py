3f32df6ffa43e1c1cda7baf933d6b5b64f823233
edomenico <67431641+edomenico@users.noreply.github.com>
2026-03-25 13:53:08 -0300

    Update tabuleiro.py

--- tabuleiro.py
+++ tabuleiro.py
@@ -769,7 +769,7 @@ def rest(areas, to_data, from_data, tipo):
         arquivo = []
         f.write(cab)
         arquivo.append(cab)
-        for k in range(0,len(estacao1))
+        for k in range(0,len(estacao1)):
             for j in range(0, intervalo + 1, 1):
                 datahinic = datahini + timedelta(days=j)
                 ano = str(datahinic)[0:4]
