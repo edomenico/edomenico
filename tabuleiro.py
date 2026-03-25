a4dcb605ae7f3ce0a24f54bfea8dfea70d9c2620
edomenico <67431641+edomenico@users.noreply.github.com>
2026-03-25 15:49:04 -0300

    Update tabuleiro.py

--- tabuleiro.py
+++ tabuleiro.py
@@ -765,7 +765,7 @@ def rest(areas, to_data, from_data, tipo):
         arquivo = []
         f.write(cab)
         arquivo.append(cab)
-        for k in range(0,len(estacaov)):
+        for k in range(0,len(estacao1)):
             for j in range(0, intervalo + 1, 1):
                 datahinic = datahini + timedelta(days=j)
                 ano = str(datahinic)[0:4]
@@ -774,7 +774,7 @@ def rest(areas, to_data, from_data, tipo):
                 datainicio = dia + '/' + mes + '/' + ano
     
                 # url = 'https://redemet.decea.gov.br//api/consulta_automatica/index.php?local=sbbr,sbgl,sbsp&msg=metar&data_ini=2023041900&data_fim=2023041923&saida_html=SIM'
-                url = 'https://api-redemet.decea.mil.br/mensagens/metar/' + estacaov[k] + '?api_key=bIV369CxQg0Zc8x0RoQPx75pXCfrBZqGZr1nfi2T'+'&msg=metar&data_ini=' + ano + mes + dia + '00&data_fim=' + ano + mes + dia + '23&saida_html=SIM&page_tam=200'
+                url = 'https://api-redemet.decea.mil.br/mensagens/metar/' + estacao1[k] + '?api_key=bIV369CxQg0Zc8x0RoQPx75pXCfrBZqGZr1nfi2T'+'&msg=metar&data_ini=' + ano + mes + dia + '00&data_fim=' + ano + mes + dia + '23&saida_html=SIM&page_tam=200'
                 print('cheguei 1')
                 print(url)
                 res = requests.get(url)
