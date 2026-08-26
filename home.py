84cac4c52982917fd72449c7adb70b738ecddd4b
edomenico <67431641+edomenico@users.noreply.github.com>
2026-08-26 13:54:49 -0300

    Update home.py

--- home.py
+++ home.py
@@ -5,7 +5,7 @@
 def home():
     import streamlit as st
     """Página Inicial do Projeto."""
-    st.title("Bem Vindo a Plataforma de Confecção de TAF")
+    st.title("Bem Vindo à Plataforma de Confecção de TAF")
     #st.image('data/logo.png')
     # st.write(
     #     "Aqui você pode acessar custos com um preço acessivél. Aprenda no Melhor Lugar!!!!"
